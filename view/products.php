51ef1e1cac5d43493c49364ea3682b56f17202ad
Unknown <alperen.kolan@students.bfh.ch>
2018-01-09 12:40:06 +0100

    ADDED First draft of products

--- view/products.php
+++ view/products.php
@@ -2,9 +2,30 @@
     <?php echo t("title"); ?>
 </h1>
 
-<?php 
-    $products = ["Auto", "Velo", "Bike"];
-    foreach($products as $product) {
-        echo "<div>$product</div><br />";
-    }
-?>
+<?php
+
+$bilderdateinamen =array();
+
+$verzeichnis = "../img";
+
+$bilderdateinamen = array(array('Produkt' => "Casio",
+                          'Preis' => "30 CHF",
+                          'Beschreibung' => "Sehr cool",
+                          'Bild' => "img/hoover.jpg") ,array('Produkt' => "Marke",
+                          'Preis' => "50 CHF",
+                          'Beschreibung' => "ehr cool",
+                          'Bild' => "img/loewen.jpg" ));
+
+// Ausgabe der Bilderdateinamen zur Kontrolle
+echo "<pre>";
+
+echo "<hr>";
+var_dump($bilderdateinamen);
+
+foreach($bilderdateinamen as $inhalt){
+      echo "<p>". $inhalt['Produkt'] .":". "</p></br>";
+      echo "<p>". $inhalt["Preis"]. "</p></br>";
+      echo "<p>". $inhalt["Beschreibung"]. "</p></br>";
+
+      echo "<img src=". $inhalt['Bild'] . ">";
+}
