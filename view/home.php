0e9d73f271c0f96e7bb598e3af878374a78ba39f
Julian <julianstampfli4@gmail.com>
2018-01-17 11:58:35 +0100

    small change

--- view/home.php
+++ view/home.php
@@ -1,3 +1,3 @@
 <h2><?php echo t("welcome"); ?></h2>
-<p> <?php echo t("pleaseLogin"); ?>Please login to order some watches. </p>
+<p> <?php echo t("pleaseLogin"); ?></p>
 <p> <?php echo t("thanks"); ?> </p>
